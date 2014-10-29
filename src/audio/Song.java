fcc975992e182b841aa0fda358dfdba05b6500f3
Samuel Kolb <samuel.kolb@me.com>
2014-10-29 14:38:09 +0100

    Added song class to wrap an audio file

---
+++ src/audio/Song.java
@@ -0,0 +1,68 @@
+package audio;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by samuelkolb on 29/10/14.
+ *
+ * @author Samuel Kolb
+ */
+public class Song {
+
+	//region Variables
+	private final String filename;
+
+	public String getFilename() {
+		return filename;
+	}
+
+	private final double length;
+
+	public double getLength() {
+		return length;
+	}
+
+	//endregion
+
+	//region Construction
+
+	/**
+	 * Creates a new song
+	 * @param filename	The filename of the audio file
+	 */
+	public Song(String filename) {
+		this.filename = filename;
+		File file = new File(filename);
+		if(!file.exists())
+			throw new IllegalArgumentException("File " + filename + " does not exist");
+		this.length = getLength(file);
+	}
+
+	//endregion
+
+	//region Public methods
+
+	/**
+	 * Returns the length of the given audio file
+	 * @param file	The audio file
+	 * @return	The length of the file
+	 */
+	public static double getLength(File file) {
+		AudioInputStream audioInputStream = null;
+		try {
+			audioInputStream = AudioSystem.getAudioInputStream(file);
+			AudioFormat format = audioInputStream.getFormat();
+			long frames = audioInputStream.getFrameLength();
+			return (frames+0.0) / format.getFrameRate();
+		} catch(Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+
+	//endregion
+}
